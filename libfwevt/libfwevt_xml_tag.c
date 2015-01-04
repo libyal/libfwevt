90fcb98e0e4fc9aa9244f21cec6bdc63f2ad968a
Joachim Metz <joachim.metz@gmail.com>
2015-01-04 15:18:35 +0100

    Applied updates.

--- libfwevt/libfwevt_xml_tag.c
+++ libfwevt/libfwevt_xml_tag.c
@@ -1,7 +1,7 @@
 /*
  * XML tag functions
  *
- * Copyright (c) 2011-2014, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2015, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
