0e9a9ae212d20bc7137aa92f3b37b5bfa62246cf
Joachim Metz <joachim.metz@gmail.com>
2016-09-22 09:21:41 +0200

    Worked on tests and Windows test scripts.

--- synclibs.ps1
+++ synclibs.ps1
@@ -1,9 +1,9 @@
 # Script that synchronizes the local library dependencies
 #
-# Version: 20160318
+# Version: 20160912
 
 $GitUrlPrefix = "https://github.com/libyal"
-$LocalLibs = @("libcdata","libcerror","libcnotify","libcstring","libcthreads","libfdatetime","libfguid","libfvalue","libuna")
+$LocalLibs = "libcdata libcerror libcnotify libcstring libcthreads libfdatetime libfguid libfvalue libuna" -split " "
 
 foreach (${LocalLib} in ${LocalLibs})
 {
--- tests/fwevt_test_memory.c
+++ tests/fwevt_test_memory.c
@@ -55,8 +55,8 @@ void *malloc(
 	if( fwevt_test_real_malloc == NULL )
 	{
 		fwevt_test_real_malloc = dlsym(
-		                           RTLD_NEXT,
-		                           "malloc" );
+		                          RTLD_NEXT,
+		                          "malloc" );
 	}
 	if( fwevt_test_malloc_attempts_before_fail == 0 )
 	{
@@ -86,8 +86,8 @@ void *memcpy(
 	if( fwevt_test_real_memcpy == NULL )
 	{
 		fwevt_test_real_memcpy = dlsym(
-		                           RTLD_NEXT,
-		                           "memcpy" );
+		                          RTLD_NEXT,
+		                          "memcpy" );
 	}
 	if( fwevt_test_memcpy_attempts_before_fail == 0 )
 	{
@@ -119,8 +119,8 @@ void *memset(
 	if( fwevt_test_real_memset == NULL )
 	{
 		fwevt_test_real_memset = dlsym(
-		                           RTLD_NEXT,
-		                           "memset" );
+		                          RTLD_NEXT,
+		                          "memset" );
 	}
 	if( fwevt_test_memset_attempts_before_fail == 0 )
 	{
@@ -151,8 +151,8 @@ void *realloc(
 	if( fwevt_test_real_realloc == NULL )
 	{
 		fwevt_test_real_realloc = dlsym(
-		                            RTLD_NEXT,
-		                            "realloc" );
+		                           RTLD_NEXT,
+		                           "realloc" );
 	}
 	if( fwevt_test_realloc_attempts_before_fail == 0 )
 	{
