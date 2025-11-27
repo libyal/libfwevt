5f64aecf8402f67eac5ba5e05fc49f046a5a8356
Joachim Metz <joachim.metz@gmail.com>
2025-11-27 21:55:21 +0100

    Applied updates

--- tests/fwevt_test_data_segment.c
+++ tests/fwevt_test_data_segment.c
@@ -1,7 +1,7 @@
 /*
  * Library data_segment type test program
  *
- * Copyright (C) 2011-2024, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2025, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
