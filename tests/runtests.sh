03308cfb026c9e83bdf689e866cd01cae5c769f1
Joachim Metz <joachim.metz@gmail.com>
2026-09-14 13:53:59 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libfwevt/.libs/libfwevt.1.dylib && test -f ./pyfwevt/.libs/pyfwevt.so
+if [ -f "${PWD}/libfwevt/.libs/libfwevt.1.dylib" ] && [ -f ./pyfwevt/.libs/pyfwevt.so ]
 then
-	install_name_tool -change /usr/local/lib/libfwevt.1.dylib ${PWD}/libfwevt/.libs/libfwevt.1.dylib ./pyfwevt/.libs/pyfwevt.so
+    install_name_tool -change /usr/local/lib/libfwevt.1.dylib "${PWD}/libfwevt/.libs/libfwevt.1.dylib" ./pyfwevt/.libs/pyfwevt.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
